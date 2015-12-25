27328896b72984a4259bb6356439ae89164ca92a
Konstantin Nesterov <wasd171@gmail.com>
2015-12-25 04:30:07 +0300

    More perfomance using batched mutations

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import Immutable, {Map} from 'immutable'
+import Immutable, {Map, OrderedMap} from 'immutable'
 import { combineReducers } from 'redux-immutablejs'
 import reduceReducers from 'reduce-reducers'
 import * as reducers from './game'
@@ -16,22 +16,6 @@ export default reduceReducers(
 
 				if (gameRunning) {
 
-					function isNeighbour(currentCell, cell) {
-						const x = currentCell.get('x');
-						const y = currentCell.get('y');
-						const nowX = cell.get('x');
-						const nowY = cell.get('y');
-						const dX = Math.abs(nowX - x);
-						const dY = Math.abs(nowY - y);
-						const bordX = ( x == 0 && nowX == size - 1 ) || ( x == size - 1 && nowX == 0 );
-						const bordY = ( y == 0 && nowY == size - 1 ) || ( y == size - 1 && nowY == 0 );
-						return (
-							( (dX <= 1) || bordX ) &&
-							( (dY <= 1) || bordY ) &&
-							( ( dX !== 0 ) || ( dY !== 0 ) )
-						)
-					}
-
 					function getNeighbours( currentCell, cellsList, includeCurrent ) {
 						const currX = currentCell.get('x');
 						const currY = currentCell.get('y');
@@ -42,69 +26,42 @@ export default reduceReducers(
 							else { return coord }
 						}
 
-						let results = Immutable.OrderedMap();
-
-						for (let dx = -1; dx <= 1; dx++) {
-							for (let dy = -1; dy <= 1; dy++) {
-								if ( !( !includeCurrent && (dx === 0) && (dy === 0) ) ) {
-									const newX = normalizeCoord(currX + dx);
-									const newY = normalizeCoord(currY + dy);
-									const key  = Map({x: newX, y: newY});
-									const value = cellsList.get(key, Map());
-									if ( value.size !== 0 ) {
-										results = results.set(key, value);
+						return OrderedMap().withMutations( (res) => {
+							for (let dx = -1; dx <= 1; dx++) {
+								for (let dy = -1; dy <= 1; dy++) {
+									if ( !( !includeCurrent && (dx === 0) && (dy === 0) ) ) {
+										const newX = normalizeCoord(currX + dx);
+										const newY = normalizeCoord(currY + dy);
+										const key  = Map({x: newX, y: newY});
+										const value = cellsList.get(key, Map());
+										if ( value.size !== 0 ) {
+											res.set(key, value);
+										}
 									}
 								}
 							}
-						}
-
-						return results;
+						});
 					}
 
-					let cellsMightChange = Immutable.OrderedMap();
 					const aliveCells = cells.filter( (cell) => cell.get('alive') );
 
-					//cells
-					//	.filter( (cell) => cell.get('alive') )
-					//	.forEach(
-					//		(aliveCell) => {
-					//			cells
-					//				.filter( (cell) => ( isNeighbour(aliveCell, cell) || (cell === aliveCell) ) )
-					//				.forEach( (cell) => {
-					//					const x = cell.get('x');
-					//					const y = cell.get('y');
-					//					cellsMightChange = cellsMightChange.set( Map({x, y}), cell )
-					//				} );
-					//		}
-					//	);
-					aliveCells.forEach( (aliveCell) => {
-						cellsMightChange = cellsMightChange.merge(getNeighbours(aliveCell, cells, true))
+					const cellsMightChange = OrderedMap().withMutations( (res) => {
+						aliveCells.forEach( (aliveCell) => {
+							res.merge( getNeighbours(aliveCell, cells, true) )
+						});
 					});
 
 					const updatedCells = cellsMightChange.map( (currentCell) => {
 						const nearSize = getNeighbours(currentCell, aliveCells, false).size;
-						//const nearSize = cellsMightChange
-						//	.filter( (cell) => cell.get('alive') )
-						//	.filter( (cell) => isNeighbour(currentCell, cell) )
-						//	.size;
 
 						return currentCell.set(
 							'alive',
-							currentCell.get('alive')
-								?
-								(nearSize === 3 || nearSize === 2 )
-								:
-								(nearSize === 3)
+							currentCell.get('alive') ? (nearSize === 3 || nearSize === 2 ) : (nearSize === 3)
 						);
 
 					});
 
 					return state.set('cells', cells.merge(updatedCells) );
-
-					//const timeEnd = Date.now();
-					//console.log('It took ', (timeEnd - timeStart), 'ms');
-
-					//return newState;
 				}
 				else {
 					return state;
